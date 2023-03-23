711a14837a25975f9f9b04c95a8c772e4f9bf5e3
Matthieu Verhust <boungii@live.fr>
2023-03-23 17:24:09 +0100

    a

---
+++ anime/component/Anime.js
@@ -0,0 +1,56 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import MangaItem from './component/Anime'
+
+export default function MangaItem({ id, title, imageUrl, saisons, episodes, detail }) {
+    const navigation = useNavigation()
+    return (
+        <View style={styles.Anime}>
+            <Pressable  style={({ pressed }) => (pressed ? styles.buttonPressed : null)} onPress={() => navigation.navigate("MealDetail", { mealId : id})}>
+                <View style={styles.innerContainer}>
+                    <View>
+                        <Image source={{ uri: imageUrl }} style={styles.image} />
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+                    <MealDetails
+                        duration={saisons}
+                        affordability={episodes}
+                        complexity={detail}
+                    />
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    Anime: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+        backgroundColor: 'white',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+    },
+    buttonPressed: {
+        opacity: 0.5,
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8,
+    },
+})
---
+++ anime/component/DetailAnime.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Detail({ saisons, episodes, details }) {
+    return (
+        <View style={styles.details}>
+            <Text style={styles.detailItem}>{saisons}m</Text>
+            <Text style={styles.detailItem}>
+                {episodes.toUpperCase()}
+            </Text>
+            <Text style={styles.detailItem}>
+                {details.toUpperCase()}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12,
+        color: "black",
+    },
+})
